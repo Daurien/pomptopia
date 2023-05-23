4ac58a6bc00f48082059d93e3d05ef5e83383320
Daurien <dorian.pierre.bessiere@gmail.com>
2023-05-23 14:18:33 -0400

    modified:   app/page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <div className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Discover & Share
+        Discover & Sharee
         <br className="max-md:hidden" />
         <span className="orange_gradient text-center">AI-Powered Prompts</span>
         <p className="desc text-center">
